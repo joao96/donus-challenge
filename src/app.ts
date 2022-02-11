ab3015ec165d8c62907d35d44968e10955bc1d20
joao96 <jvpoletti@gmail.com>
2022-02-11 13:59:27 -0300

    🎉 setting up

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import 'reflect-metadata';
+import cors from 'cors';
+import express, { NextFunction, Response, Request } from 'express';
+
+import 'express-async-errors';
+import { AppError } from './errors/AppError';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(
+  (err: Error, request: Request, response: Response, _next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({ message: err.message });
+    }
+
+    return response.status(500).json({
+      status: 'Error',
+      message: `Internal server error ${err.message}`,
+    });
+  }
+);
+
+export { app };
